7a5f87bf8daff2e3714b6f8ae9695bc662c281c0
pradeep mutham <pradeep.mutham161814@gmail.com>
2025-10-22 16:45:56 +0530

    basic singleton example

---
+++ src/main/java/org/learn/designpatterns/creational/singleton/Singleton.java
@@ -0,0 +1,17 @@
+package org.learn.designpatterns.creational.singleton;
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    // private constructor to prevent instantiation
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
